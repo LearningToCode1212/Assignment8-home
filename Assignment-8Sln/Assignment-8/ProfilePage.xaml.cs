abaff16291b6d5adda8550604e800b35e1fe4aa4
LearningToCode1212 <124621733+LearningToCode1212@users.noreply.github.com>
2024-03-19 17:12:34 +0200

    Profile table and Page done

---
+++ Assignment-8Sln/Assignment-8/ProfilePage.xaml.cs
@@ -0,0 +1,53 @@
+using Assignment_8.Models;
+using Assignment_8.Services;
+
+namespace Assignment_8;
+
+public partial class ProfilePage : ContentPage
+{
+    private ProfileDatabase _database;
+
+    private UserProfile _currentUser;
+    public UserProfile CurrentUser
+    {
+        get { return _currentUser; }
+        set
+        {
+            _currentUser = value;
+            OnPropertyChanged();
+        }
+    }
+    public ProfilePage()
+	{
+		InitializeComponent();
+        _database = new ProfileDatabase();
+        BindingContext = this;
+    }
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        LoadData();
+    }
+    public void LoadData() // Loading the data
+    {
+        UserProfile profile = _database.GetUserByID(1); // Assigning the user to a userid that was saved
+        CurrentUser = profile; // Assigning the user to the CurrentUser (will display on screen)
+    }
+
+    private void SaveProfile(object sender, EventArgs e)
+    {
+        //DisplayAlert("test", "button click works", "ok");
+        _database.UpdateUser(CurrentUser); // Saving the data to the database but updating with new information
+        nameEntry.Text = "";
+        surnameEntry.Text = "";
+        emailEntry.Text = "";
+        bioEntry.Text = "";
+    }
+
+    private void LoadProfile(object sender, EventArgs e) // Loading data on button clcik
+    {
+        //DisplayAlert("test", "is working", "ok");
+        LoadData();
+    }
+}
--- Assignment-8Sln/Assignment-8/Services/ProfileDatabase.cs
+++ Assignment-8Sln/Assignment-8/Services/ProfileDatabase.cs
@@ -1,4 +1,6 @@
-﻿using SQLite;
+﻿using Assignment_8.Models;
+using SQLite;
+using SQLiteNetExtensions.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +21,24 @@ namespace Assignment_8.Services
         public ProfileDatabase()
         {
             _dbConnection = new SQLiteConnection(GetDatabasePath());
+            _dbConnection.CreateTable<UserProfile>();
+        }
+
+
+        //Methods
+        public void UpdateUser(UserProfile profile)
+        {
+            _dbConnection.Update(profile); // Updating the database with new information passed through
+        }
+
+        public UserProfile GetUserByID(int id) // Getting the ID of the user
+        {
+            UserProfile user = _dbConnection.Table<UserProfile>().Where(x => x.UserId == id).FirstOrDefault();
+
+            if (user != null)
+                _dbConnection.GetChildren(user, true);
+
+            return user;
         }
     }
 }
