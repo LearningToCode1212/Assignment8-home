de2f680e87410b053dbc2b3feab5cfe397a1939b
LearningToCode1212 <124621733+LearningToCode1212@users.noreply.github.com>
2024-03-19 17:02:43 +0200

    Database Created

---
+++ Assignment-8Sln/Assignment-8/Services/ProfileDatabase.cs
@@ -0,0 +1,24 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_8.Services
+{
+    internal class ProfileDatabase
+    {
+        private SQLiteConnection _dbConnection;
+        public string GetDatabasePath()
+        {
+            string fileName = "profiledatabase.db";
+            string pathToDb = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            return pathToDb + fileName;
+        }
+        public ProfileDatabase()
+        {
+            _dbConnection = new SQLiteConnection(GetDatabasePath());
+        }
+    }
+}
